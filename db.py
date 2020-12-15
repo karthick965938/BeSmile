eed45c954b8ce50eebca3f76ba9601f19138f1dc
Karthick Nagarajan <karthick965938@gmail.com>
2020-12-15 22:14:07 +0530

    Connect GridDB

--- db.py
+++ db.py
@@ -1,9 +1,24 @@
-import sqlite3
+import jaydebeapi
+import sys
 
-conn = sqlite3.connect('DB/database.db')
-print("Opened database successfully")
+argv = sys.argv
 
-conn.execute('CREATE TABLE IF NOT EXISTS emotions (name text, create_date date, created_at timestamp)')
-conn.execute('CREATE TABLE IF NOT EXISTS user (email_address text, created_at timestamp)')
-print("Table created successfully")
+multicast_address = argv[1] # default : 239.0.0.1
+port_no = argv[2] # default : 41999
+cluster_name = argv[3]
+username = argv[4]
+password = argv[5]
+
+url = "jdbc:gs://" + multicast_address + ":" + port_no + "/" + cluster_name
+conn = jaydebeapi.connect("com.toshiba.mwcloud.gs.sql.Driver", url, [username, password], "./gridstore-jdbc.jar")
+
+curs = conn.cursor()
+
+curs.execute("DROP TABLE IF EXISTS Emotion")
+curs.execute("CREATE TABLE IF NOT EXISTS Emotion ( name text, create_date date, created_at timestamp)")
+print('SQL Create Table name=Emotion')
+curs.execute("CREATE TABLE IF NOT EXISTS User ( email_address text, created_at timestamp)")
+print('SQL Create Table name=User')
+curs.close()
 conn.close()
+print('success!')
--- main.py
+++ main.py
@@ -7,13 +7,25 @@ import os
 import sqlite3 as sql
 import notify2
 import random
+import jaydebeapi
+import sys
+
+argv = sys.argv
+
+multicast_address = argv[1] # default : 239.0.0.1
+port_no = argv[2] # default : 41999
+cluster_name = argv[3]
+username = argv[4]
+password = argv[5]
+
+url = "jdbc:gs://" + multicast_address + ":" + port_no + "/" + cluster_name
+conn = jaydebeapi.connect("com.toshiba.mwcloud.gs.sql.Driver", url, [username, password], "./gridstore-jdbc.jar")
 
 app = Flask(__name__)
 
 def get_user_data():
-  con = sql.connect("DB/database.db")
-  cur = con.cursor()
-  cur.execute("select * from user")
+  cur = conn.cursor()
+  cur.execute("SELECT * from User")
   user = cur.fetchall();
   if len(user) > 0 :
     return True
@@ -41,11 +53,10 @@ def index():
 def userDetailStore():
   email =  request.form['email'];
   time = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
-  try: 
-    with sql.connect("DB/database.db") as con:
-      cur = con.cursor()
-      cur.execute("INSERT INTO user (email_address,created_at) VALUES (?,?)",(email, time))
-      con.commit()
+  try:
+    cur = conn.cursor()
+    cur.execute("INSERT INTO User (email_address,created_at) VALUES (?,?)",(email, time))
+    con.commit()
   except:
     con.rollback()
   finally:
@@ -56,23 +67,22 @@ def userDetailStore():
 def getEmotionCount():
   user = os.environ['USER'].title()
   date = datetime.now().strftime("%d-%m-%Y")
-  con = sql.connect("DB/database.db")
-  neutral = con.cursor()
+  neutral = conn.cursor()
   neutral.execute("SELECT name FROM emotions WHERE name=? AND create_date=?", ('Neutral', (request.form['data'], date)[request.form['data'] == 'today']))
   neutral_count = neutral.fetchall()
-  happy = con.cursor()
+  happy = conn.cursor()
   happy.execute("SELECT name FROM emotions WHERE name=? AND create_date=?", ('Happy', (request.form['data'], date)[request.form['data'] == 'today']))
   happy_count = happy.fetchall()
-  sad = con.cursor()
+  sad = conn.cursor()
   sad.execute("SELECT name FROM emotions WHERE name=? AND create_date=?", ('Sad', (request.form['data'], date)[request.form['data'] == 'today']))
   sad_count = sad.fetchall()
-  angry = con.cursor()
+  angry = conn.cursor()
   angry.execute("SELECT name FROM emotions WHERE name=? AND create_date=?", ('Angry', (request.form['data'], date)[request.form['data'] == 'today']))
   angry_count = angry.fetchall()
-  fear = con.cursor()
+  fear = conn.cursor()
   fear.execute("SELECT name FROM emotions WHERE name=? AND create_date=?", ('Fear', (request.form['data'], date)[request.form['data'] == 'today']))
   fear_count = fear.fetchall()
-  surprise = con.cursor()
+  surprise = conn.cursor()
   surprise.execute("SELECT name FROM emotions WHERE name=? AND create_date=?", ('Surprise', (request.form['data'], date)[request.form['data'] == 'today']))
   surprise_count = surprise.fetchall()
   return json.dumps({'Neutral':len(neutral_count), 'Happy':len(happy_count), 'Sad':len(sad_count), 'Angry':len(angry_count), 'Fear':len(fear_count), 'Surprise':len(surprise_count)});
